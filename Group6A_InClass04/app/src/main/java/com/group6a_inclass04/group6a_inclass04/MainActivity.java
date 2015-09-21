0ea82b6a635d077f3468fd111de12b111e8e7c32
Arunkumar <b.arun410@gmail.com>
2015-09-21 19:00:16 -0400

    Main Activity get list

--- Group6A_InClass04/app/src/main/java/com/group6a_inclass04/group6a_inclass04/MainActivity.java
+++ Group6A_InClass04/app/src/main/java/com/group6a_inclass04/group6a_inclass04/MainActivity.java
@@ -1,17 +1,89 @@
 package com.group6a_inclass04.group6a_inclass04;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    static ArrayList<String> id = new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Test comment
+
+        if(connectedOnline()){
+            new GetList().execute("http://dev.theappsdr.com/lectures/inclass_photos/index.php");
+        }
+
+    }
+
+    private boolean connectedOnline(){
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = cm.getActiveNetworkInfo();
+
+        if(info!=null && info.isConnected()){
+            return true;
+        }
+        else return false;
+    }
+
+    private class GetList extends AsyncTask<String,Void,ArrayList<String>>{
+        protected BufferedReader reader=null;
+
+        @Override
+        protected ArrayList<String> doInBackground(String... params) {
+            URL url = null;
+            try {
+                url = new URL(params[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+
+                reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                ArrayList<String> idList = new ArrayList<String>();
+                String line = "";
+
+                while((line = reader.readLine()) != null){
+                    idList.add(line);
+                }
+                return idList;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<String> idList) {
+            super.onPostExecute(idList);
+
+            for(String photoId:idList){
+                id.add(photoId);
+
+                Log.d("Phot_ID",photoId);
+            }
+        }
     }
 
     @Override
